e8766f583e9c27c87e3be77ec9d87d20f2c73555
jiasen <245987430@qq.com>
2020-10-21 00:13:18 +0800

    update .gitignore

--- src/main/java/com/jiasen/controller/EmployeeController.java
+++ src/main/java/com/jiasen/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jiasen.bean.Employee;
 import com.jiasen.service.EmployeeService;
@@ -16,9 +17,10 @@ public class EmployeeController {
 	EmployeeService employeeService;
 
 	@RequestMapping("/getall")
-	public String getAll() {
+	public String getAll(@RequestParam(value = "pn", defaultValue = "1") Integer pn) {
+		// 分页
 		List<Employee> list = employeeService.getAll();
-		System.out.println(list);
+		System.out.println(list.get(0).geteName());
 		return "list";
 	}
 
